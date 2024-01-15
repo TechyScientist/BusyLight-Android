afac98e739ee182f1e64a7e4b172a90feb4c870e
Johnny Console <johnny.console@hotmail.com>
2024-01-15 09:29:48 -0500

    Set Minimum API to 31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "net.johnnyconsole.android.busylight"
-        minSdk = 27
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
